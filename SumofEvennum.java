9f4167911b279029efd0380e0a716b00d1baed73
punnammanikanta <PUNNAMMANIKANTA52@GMAIL.COM>
2025-01-30 12:55:37 +0530

    Add files via upload

---
+++ SumofEvennum.java
@@ -0,0 +1,13 @@
+public class SumofEvennum{
+    public static void main (String[] args){
+        int result=0;
+        for(int i =0;i<=100;i=i+2){
+            result=result+i;
+            System.out.println(result);
+        }
+    }
+    
+}
+
+    
+
